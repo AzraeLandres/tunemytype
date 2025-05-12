8b9e94bc7b95473188d9ffe1d57fb2218e64bd25
Azrael Landres <azraellandres@gmail.com>
2025-05-12 23:40:53 +0200

    chore: add path aliases to Vite config for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import Components from "unplugin-vue-components/vite";
 import RadixVueResolver from "radix-vue/resolver";
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,4 +14,14 @@ export default defineConfig({
       resolvers: [RadixVueResolver()],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@router": path.resolve(__dirname, "./src/router"),
+    },
+  },
 });
